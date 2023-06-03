7d4f96b419e189fc0b72cdcddcf945b904c35c03
Haibin Zhao <zhb950102@qq.com>
2023-06-03 23:20:55 +0200

    update datasets

--- psnn.py
+++ psnn.py
@@ -1,170 +1,179 @@
 import torch
-import torch.nn as nn
 
+#===============================================================================
+#============================ Single Spiking Neuron ============================
+#===============================================================================
 
-class SpikingNeuron(nn.Module):
+class SpikingNeuron(torch.nn.Module):
+    def __init__(self, beta=None, threshold=None, random_state=True):
+        super().__init__()
+        
+        # initialize beta
+        if beta is None:
+            beta = torch.rand(1)
+        beta_ = torch.log(beta / (1 - beta))
+        self.beta_ = torch.nn.Parameter(beta_, requires_grad=True)
+        
+        # initialize threshold
+        if threshold is None:
+            threshold = torch.rand(1)
+        self.threshold = torch.nn.Parameter(threshold)
 
-    instances = []
+        # initialize initial state, becase the shape must be coordinate with batch size
+        # it can be re-initialized by calling self.ResetHidden()
+        self.init_memory = False
+        # whether to initialize the initial state randomly for simulating unknown previous state
+        # this is especially useful for signals split by sliding windows
+        self.random_state = random_state
 
-    def __init__(
-        self,
-        threshold=1.0,
-        init_hidden=False
-    ):
-        super(SpikingNeuron, self).__init__()
-
-        SpikingNeuron.instances.append(self)
-        self.init_hidden = init_hidden
-        self.threshold = nn.Parameter(threshold)
-
-    def spike_grad(self, mem_shift):
-        forward = (mem_shift > 0).float()
-        backward = torch.sigmoid(mem_shift * 5.)
-        return forward.detach() + backward - backward.detach()
-
-    def fire(self, mem):
-        mem_shift = mem - self.threshold
-        spk = self.spike_grad(mem_shift)
-        return spk
-
-    def mem_reset(self, mem):
-        mem_shift = mem - self.threshold
-        reset = self.spike_grad(mem_shift).clone().detach()
-        return reset
-
-
-
-class LIF(SpikingNeuron):
-    def __init__(
-        self,
-        beta,
-        threshold=1.0,
-        init_hidden=False
-    ):
-        super().__init__(
-            threshold,
-            init_hidden
-        )
-
-        self.beta = nn.Parameter(beta)
-
-    @staticmethod
-    def init_leaky():
-        mem = _SpikeTensor(init_flag=False)
-
-        return mem
-
-
-class _SpikeTensor(torch.Tensor):
-    """Inherits from torch.Tensor with additional attributes.
-    ``init_flag`` is set at the time of initialization.
-    When called in the forward function of any neuron, they are parsed and
-    replaced with a torch.Tensor variable.
-    """
-
-    @staticmethod
-    def __new__(cls, *args, init_flag=False, **kwargs):
-        return super().__new__(cls, *args, **kwargs)
-
-    def __init__(
-        self,
-        *args,
-        init_flag=True,
-    ):
-        self.init_flag = init_flag
-
-
-def _SpikeTorchConv(*args, input_):
-    """Convert SpikeTensor to torch.Tensor of the same size as ``input_``."""
-
-    states = []
-    if (
-        len(args) == 1 and type(args) is not tuple
-    ):  # if only one hidden state, make it iterable
-        args = (args,)
-    for arg in args:
-        arg = arg.to("cpu")
-        arg = torch.Tensor(arg)  # wash away the SpikeTensor class
-        arg = torch.zeros_like(input_, requires_grad=True)
-        states.append(arg)
-    if len(states) == 1:  # otherwise, list isn't unpacked
-        return states[0]
-
-    return states
-
-
-class Leaky(LIF):
-
-    def __init__(
-        self,
-        beta,
-        threshold=1.0,
-        init_hidden=False
-    ):
-        super(Leaky, self).__init__(
-            beta,
-            threshold,
-            init_hidden
-        )
-
-        if self.init_hidden:
-            self.mem = self.init_leaky()
-
-    def forward(self, input_, mem=False):
-
-        if hasattr(mem, "init_flag"):  # only triggered on first-pass
-            mem = _SpikeTorchConv(mem, input_=input_)
-        elif mem is False and hasattr(
-            self.mem, "init_flag"
-        ):  # init_hidden case
-            self.mem = _SpikeTorchConv(self.mem, input_=input_)
-            print('init_hidden', self.init_hidden)
-            print('mem', self.mem)
-
-
-        if not self.init_hidden:
-            self.reset = self.mem_reset(mem)
-            mem = self._build_state_function(input_, mem)
-            spk = self.fire(mem)
-
-            return spk, mem
-
-        # intended for truncated-BPTT where instance variables are hidden
-        # states
-        if self.init_hidden:
-            self._leaky_forward_cases(mem)
-            self.reset = self.mem_reset(self.mem)
-            self.mem = self._build_state_function_hidden(input_)
-
-
-            self.spk = self.fire(self.mem)
-
-            return self.spk, self.mem
-
-
-    def _base_state_function(self, input_, mem):
-        base_fn = self.beta.clamp(0, 1) * mem + input_
-        return base_fn
-
-    def _build_state_function(self, input_, mem):
-        state_fn = self._base_state_function(
-            input_, mem - self.reset * self.threshold
-        )
-        return state_fn
-
-    def _base_state_function_hidden(self, input_):
-        base_fn = self.beta.clamp(0, 1) * self.mem + input_
-        return base_fn
-
-    def _build_state_function_hidden(self, input_):
-        state_fn = (
-            self._base_state_function_hidden(input_)
-            - self.reset * self.threshold
-        )
-        return state_fn
+    @property
+    def beta(self):
+        # keep beta in range (0, 1)
+        return torch.sigmoid(self.beta_)
     
-    def _leaky_forward_cases(self, mem):
-        if mem is not False:
-            raise TypeError(
-                "When `init_hidden=True`, Leaky expects 1 input argument."
-            )
+    def SpikeGenerator(self, surplus):
+        # straight through estimator
+        # forward is 0/1 spike
+        forward = (surplus >= 0).float()
+        # backward is sigmoid relaxation
+        backward = torch.sigmoid(surplus * 5.)
+        return forward.detach() + backward - backward.detach()
+    
+    def StateUpdate(self, x):
+        # update the state of neuron with new input
+        return self.beta * self.memory + x
+    
+    @property
+    def fire(self):
+        # detect whether the neuron fires
+        return self.SpikeGenerator(self.memory - self.threshold)
+    
+    def StatePostupdate(self):
+        # update the state of neuron after firing
+        return self.memory - self.fire.clone().detach() * self.threshold    
+
+    def SingleStepForward(self, x):
+        self.memory = self.StateUpdate(x)
+        self.spike = self.fire
+        self.memory = self.StatePostupdate()
+        return self.spike, self.memory
+    
+    def forward(self, x):
+        N_batch, T = x.shape
+        # initialize the initial memory to match the batch size
+        if not self.init_memory:
+            if self.random_state:
+                self.memory = torch.rand(N_batch) * self.threshold.detach()
+            else:
+                self.memory = torch.zeros(N_batch)
+            self.init_memory = True
+        # forward
+        spikes = []
+        memories = [self.memory] # add initial state
+        for t in range(T):
+            spike, memory = self.SingleStepForward(x[:,t])
+            spikes.append(spike)
+            memories.append(memory)
+        memories.pop() # remove the last one to keep the same length as spikes
+        # output
+        return torch.stack(spikes, dim=1), torch.stack(memories, dim=1)
+
+    def ResetHidden(self):
+        # after calling this function, the initial state will be re-initialized
+        # can be used e.g., inbetween traning, validating, and testing
+        self.init_memory = False
+
+#===============================================================================
+#========================= Spiking Neurons in a Layer ==========================
+#===============================================================================
+
+class SpikingLayer(torch.nn.Module):
+    def __init__(self, N_neuron, beta=None, threshold=None, random_state=True, spike_only=True):
+        super().__init__()
+        # create a list of neurons, number of neurons is the number of channels
+        self.SNNList = torch.nn.ModuleList()
+        for n in range(N_neuron):
+            self.SNNList.append(SpikingNeuron(beta, threshold, random_state))
+        # whether to output only spikes, False is geneally for visualization
+        self.spike_only = spike_only
+    
+    def forward(self, x):
+        spikes = []
+        memories = []
+        # for each channel, run the corresponding spiking neuron
+        for c in range(x.shape[1]):
+            spike, memory = self.SNNList[c](x[:,c,:])
+            spikes.append(spike)
+            memories.append(memory)
+        if self.spike_only:
+            return torch.stack(spikes, dim=1)
+        else:
+            return torch.stack(spikes, dim=1), torch.stack(memories, dim=1)
+    
+    def ResetHidden(self):
+        # reset the initial state of all neurons in the layer
+        for snn in self.SNNList:
+            snn.ResetHidden()
+    
+    def ResetOutput(self, spike_only):
+        # reset the output mode
+        self.spike_only = spike_only
+
+#===============================================================================
+#==================== Weighted-sum for Temporal Signal =========================
+#===============================================================================
+
+class TemporalWeightedSum(torch.nn.Module):
+    def __init__(self, N_in, N_out):
+        super().__init__()
+        # this paameter contrains both weights and bias
+        self.weight = torch.nn.Parameter(torch.rand(N_in+1, N_out)/20.)
+    
+    def forward(self, x):
+        # extend the input with 1 for bias
+        x_extend = torch.cat([x, torch.ones(x.shape[0], 1, x.shape[2])], dim=1)
+        # for each time step, compute the weighted sum
+        T = x.shape[2]
+        result = []
+        for t in range(T):
+            mac = torch.matmul(x_extend[:, :, t], self.weight)
+            result.append(mac)
+        return torch.stack(result, dim=2)
+    
+#===============================================================================
+#======================== Spiking Neural Network ===============================
+#===============================================================================
+
+class SpikingNeuralNetwork(torch.nn.Module):
+    def __init__(self, topology, beta=None, threshold=None, random_state=True, spike_only=True):
+        super().__init__()
+        # create snn with weighted-sum and spiking neurons
+        self.model = torch.nn.Sequential()
+        for i in range(len(topology)-1):
+            self.model.add_module('MAC'+str(i), TemporalWeightedSum(topology[i], topology[i+1]))
+            self.model.add_module('SNNLayer'+str(i), SpikingLayer(topology[i+1], beta, threshold, random_state))
+        # in the complete spiking neural network, the output should be spike only
+        self.spike_only = spike_only
+
+    def forward(self, x):
+        return self.model(x)
+    
+    def ResetHidden(self):
+        for layer in self.model:
+            if hasattr(layer, 'ResetHidden'):
+                layer.ResetHidden()
+    
+    def ResetOutput(self, spike_only, Layer=None):
+        self.spike_only = spike_only
+        if Layer is None:
+            for layer in self.model:
+                if hasattr(layer, 'ResetOutput'):
+                    layer.ResetOutput(spike_only)
+        else:
+            for l, layer in enumerate(self.model):
+                if l==Layer:
+                    try:
+                        layer.ResetOutput(spike_only)
+                        print(f"Reset output of layer {str(Layer)}.")
+                    except:
+                        print(f"Layer {str(Layer)} is not a spiking layer, it has no attribute 'ResetOutput'.")
